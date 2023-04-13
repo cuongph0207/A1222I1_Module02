bb377180936be553dfad2dca913cb87810a1c207
cuongph0207 <cuongphktos2003@gmail.com>
2023-04-14 01:41:49 +0700

    update

---
+++ baitap/ss8_clean_code/refactoring/Main.java
@@ -0,0 +1,10 @@
+package module2.baitap.ss8_clean_code.refactoring;
+
+public class Main {
+    public static void main(String[] args) {
+        TennisGame tennis = new TennisGame();
+        System.out.println("The Game On is :" + tennis.gameOn("Cuong", "Cu", 5,
+                5));
+
+    }
+}
